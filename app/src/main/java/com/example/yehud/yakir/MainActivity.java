7cd6b8967652b2dfcf02eefcaad49c29c4a1784f
livnoni <yehuda.livnoni@gmail.com>
2017-07-27 00:11:39 +0300

    Added parsing to all the tables data.

--- app/src/main/java/com/example/yehud/yakir/MainActivity.java
+++ app/src/main/java/com/example/yehud/yakir/MainActivity.java
@@ -18,18 +18,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     TextView textView;
-    Tfila tfila;
     private ProgressDialog mProgressDialog;
 
 
-    String docUrl = "https://docs.google.com/document/d/1SjlZiydYFpwctaTzcuVGx3C1tVT3DqFX-AOQh4vhHiw/pub";
     String sheetUrl = "https://docs.google.com/spreadsheets/d/1AercbZdDUV5AhMFT7YTCHLsHGzxmY1HCynpt9qw-zyM/pubhtml#";
+    DataObject dataObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,35 +47,47 @@ public class MainActivity extends AppCompatActivity {
         mProgressDialog.setMessage("טוען זמני תפילות");
 
 
+        dataObject = new DataObject();
+        new grabData().execute();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new doit().execute();
+                new grabData().execute();
             }
         });
 
     }
 
 
-    public class doit extends AsyncTask<Void,Void,Void>{
-        String words;
-
+    public class grabData extends AsyncTask<Void,Void,Void>{
+        String[][] trtd;
         @Override
         protected Void doInBackground(Void... params) {
             try {
                 Document doc = Jsoup.connect(sheetUrl).get();
-                Elements table = doc.select("table[class=waffle]");
+                Elements tables = doc.select("table[class=waffle]");
+                for (Element table : tables) {
+                    Elements trs = table.select("tr");
+                    trtd = new String[trs.size()][];
+                    for (int i = 0; i < trs.size(); i++) {
+                        Elements tds = trs.get(i).select("td");
+                        trtd[i] = new String[tds.size()];
+                        for (int j = 0; j < tds.size(); j++) {
+                            trtd[i][tds.size()-j-1] = tds.get(j).text();
+//                            trtd[i][j] = tds.get(j).text();
+                        }
+                    }
+                    // trtd now contains the desired array for this table
+                    for(int i=0; i<trtd.length; i++)
+                    {
+                        Log.d("trtd["+i+"]", Arrays.toString(trtd[i]));
+                    }
+                    dataObject.addMinyan(trtd);
 
-                Iterator<Element> iterator = table.select("tr").iterator();
-                int iter =1;
-                while(iterator.hasNext()){
-                    iter++;
-                    String line = iterator.next().text();
-                    words = words +line +"\n";
-                    Log.i("JSOUP",line);
                 }
 
+                Log.d("grabData","FINISH------------------------------------------");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -85,10 +98,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            textView.setText(words);
-            tfila = new Tfila((words));
             mProgressDialog.dismiss();
-
+            dataObject.printData();
 
         }
 
@@ -97,9 +108,55 @@ public class MainActivity extends AppCompatActivity {
             mProgressDialog.show();
         }
 
+    }
 
+    public class DataObject
+    {
+        ArrayList<String[][]> minyanArrayList;
 
+        public DataObject()
+        {
+            minyanArrayList = new ArrayList<String[][]>();
+        }
+        public void addMinyan(String [][] minyan)
+        {
+            minyanArrayList.add(minyan);
+        }
+        public void printData()
+        {
+            for(int i=0; i<minyanArrayList.size(); i++){
+                for(int j=0; j<minyanArrayList.get(i).length; j++){
+                    Log.d("Minyan_"+i,Arrays.toString(minyanArrayList.get(i)[j]));
+                }
+            }
+        }
 
+//        public class Minyan
+//        {
+//            String name;
+//            Times weekday;
+//            Times saturday;
+//
+//            public Minyan(Times weekday, Times saturday)
+//            {
+//                this.weekday = weekday;
+//                this.saturday = saturday;
+//            }
+//        }
+//        public class Times
+//        {
+//            String [] morningPray;
+//            String [] middlePray;
+//            String [] eveningPray;
+//
+//            public Times(String [] morningPray ,String [] middlePray, String [] eveningPray)
+//            {
+//                this.morningPray = morningPray;
+//                this.middlePray = middlePray;
+//                this.eveningPray = eveningPray;
+//            }
+//
+//        }
 
     }
 }
